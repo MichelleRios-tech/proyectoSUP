75253ec37842bdb8e516f65c4811d7d7d496f8cb
eionbk <gamma-1994@hotmail.com>
2021-09-07 19:40:07 -0500

    agregada imagen

--- project_jikan/src/componentes/BarraBusqueda/busqueda.js
+++ project_jikan/src/componentes/BarraBusqueda/busqueda.js
@@ -2,10 +2,10 @@ import React from "react";
 import s from "./busqueda.module.css"
 
 function Buscar(props){
-    return (
+    return (    
         <>
-            <input type="text" />
-            <button> Buscar <button>
+            <input className={s.i} type="text" />
+            <button>Buscar</button>
         </>
     )
 
--- project_jikan/src/componentes/welcomePage/welcome.js
+++ project_jikan/src/componentes/welcomePage/welcome.js
@@ -3,16 +3,19 @@ import s from "./welcome.module.css";
 
 
 
+
 function Welcome(props){
   return (
-    <div>
-      <h1>Hola Bienvenido!</h1>
-      <p>
-        Este es el proyecto que iniciamos de la mano de nuestro T.A.S de HENRY.
-        el cual nos va servir para practicar nustro preyecto individual.
-        
-        ¡Vamos con toda!
-      </p>
+    <div className={s.contenedor}>
+      <div className={s.bienvenida}>
+        <h1>Hola Bienvenido!</h1>
+        <p>
+          Este es el proyecto que iniciamos de la mano de nuestro T.A.S de HENRY.
+          el cual nos va servir para practicar nustro preyecto individual.
+          
+          ¡Vamos con toda!
+        </p>
+      </div>
     </div>
   )
 }
